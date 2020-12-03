7e59545924c0805eccc80f7e3609411bcacc5f8e
ellavrubel <webella@protonmail.com>
2020-12-03 11:35:04 +0300

    modals finished

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,24 +1,35 @@
 
 
 // функция для всех модальных окон
+
 const modals = () => {
     
-    function bindModal(trigger, modal, close) { // триггер - кнопка; само модальное окно и триггер для его закрытия
+    function bindModal(triggerSelector, modalSelector, closeSelector) { // триггер - кнопка; само модальное окно и триггер для его закрытия
 
-        trigger.addEventListener('click', (e) => {  // e - event, например не кнопка, а ссылка, кот перезагрузит страницу, а нам это не нужно
-            if (e.target){
-                e.preventDefault();
-            }
+        const trigger = document.querySelectorAll(triggerSelector);
+        const modal = document.querySelector(modalSelector);
+        const close = document.querySelector(closeSelector);
 
-            modal.style.display = 'block';
-            // document.body.style.overflow = 'hidden'; // при открытом модальном окне страница сайта будет замораживаться
-            document.body.classList.add('modal-open');
+
+
+        trigger.forEach(item => {  // forEach возможен только при querySelectorAll и на каждый элемент массива с одинаковыми классами будет навешан обработ событий
+
+            item.addEventListener('click', (e) => {  // e - event, например не кнопка, а ссылка, кот перезагрузит страницу, а нам это не нужно
+                if (e.target){
+                    e.preventDefault();
+                }
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden'; // при открытом модальном окне страница сайта будет замораживаться
+                // document.body.classList.add('modal-open'); // т.к. в проекте есть Bootstrap
+            });
+
         });
 
         close.addEventListener('click', () => {
             modal.style.display = 'none';
-            // document.body.style.overflow = '';
-            document.body.classList.remove('modal-open');
+            document.body.style.overflow = '';
+            // document.body.classList.remove('modal-open');
 
         });
 
@@ -26,21 +37,28 @@ const modals = () => {
 
             if (e.target === modal){
                 modal.style.display = 'none';
-                // document.body.style.overflow = '';
-                document.body.classList.remove('modal-open');
+                document.body.style.overflow = '';
+                // document.body.classList.remove('modal-open');
             }
 
         })
 
-
-
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn');
-    const modalEngineer = document.querySelector('.popup_engineer');
-    const modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+    function showModalByTime (selector, time){
+        setTimeout(() => {
 
-    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
+
+        }, time)
+    }
+
+
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+    showModalByTime('.popup', 60000);
 
 };
 
